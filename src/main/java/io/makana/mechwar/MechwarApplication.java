44f4cdd3626bc5a653a100be18d0629f9259b1fa
Branden Makana <branden.makana@gmail.com>
2018-08-13 21:33:00 -0700

    Initial shell

---
+++ src/main/java/io/makana/mechwar/MechwarApplication.java
@@ -0,0 +1,12 @@
+package io.makana.mechwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechwarApplication.class, args);
+	}
+}
